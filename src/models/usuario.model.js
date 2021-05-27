06c13c1120b6528bfbd47a2382585056d7b8a4c1
david <davidbgemin@gmail.com>
2021-05-27 17:19:07 -0500

    models, mongodb y conf de variables de entorno

---
+++ src/models/usuario.model.js
@@ -0,0 +1,50 @@
+import { Schema, Types } from "mongoose";
+import { tareaSchema } from "./tarea.models";
+
+const usuarioSchema = new Schema({
+    usuarioNombre: {
+        type: Schema.Types.String,
+        alias: 'nombre'
+    },
+    usuarioApellido: {
+        type: Schema.Types.String,
+        alias: 'apellido', // sirve para modificar el nombre de la columna en la bd
+        required: true, // indica que el campo no puede tener un valor nulo
+        // adicionalmente a las caracterísiticas previas podemos indicar algunas adicionales dependiendo del tipo de dato de la columna
+        // lowercase: true,
+        trim: true, // elimina los espacios al comienzo y al final del texto
+        minLength: 5, //longitud mínima del texto
+        maxLength: 30, //longitud máxima del texto
+    },
+    usuarioCorreo: {
+        unique: true, // nunca se puede ingresar el mismo correo dos veces o más
+        type: Schema.Types.String,
+        required: true,
+        alias: 'correo'
+    },
+    usuarioPassword: {
+        type: Schema.Types.String,
+        alias: 'password',
+    },
+    usuarioCelular: {
+        type: Schema.Types.Number,
+        unique: true,
+        alias: "celular",
+    },
+    usuarioFechaNacimiento: {
+        type: Schema.Types.Date,
+        alias: "fec_nacimiento",
+    },
+
+    // para relación nosql con la colección tarea:
+    usuarioTareas: [tareasSchema],
+    alias: "tareas"
+},
+{
+    timestamps: {
+        createdAt: "fecha_creacion",
+        updatedAt: false
+    }
+});
+
+export const Usuario = model("usuario", usuarioSchema);
